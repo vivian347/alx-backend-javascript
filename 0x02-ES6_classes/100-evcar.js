59e2815d6daa985ca823c5ecd7832f0fa3f8c2ac
vivian347 <vkinyua001@gmail.com>
2023-03-25 12:59:24 +0300

    100

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-underscore-dangle */
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    return new Car(this._brand, this._color, this._motor);
+  }
+}
