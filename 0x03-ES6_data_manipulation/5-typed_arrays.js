91c892dcbc240672917fe3139b3870f718ca660e
vivian347 <vkinyua001@gmail.com>
2023-03-28 13:37:40 +0300

    0-5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
-    if ((position < 0) && (position > length)) {
-        throw new Error('Position outside range');
-    }
-    const buffer = new ArrayBuffer(length);
+  if ((position < 0) && (position > length)) {
+    throw new Error('Position outside range');
+  }
+  const buffer = new ArrayBuffer(length);
 
-    const int8Array = new Int8Array(buffer, 0, length);
-    int8Array[position] = value;
+  const int8Array = new Int8Array(buffer, 0, length);
+  int8Array[position] = value;
 
-    return buffer;
+  return buffer;
 }
