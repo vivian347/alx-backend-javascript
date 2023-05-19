ee7c10736dd1d3122112108fc7dc28b8ec078242
vivian347 <vkinyua001@gmail.com>
2023-05-19 05:40:02 +0300

    unittest in js

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,29 @@
+const sendPaymentRequestToApi = require('./5-payment');
+const sinon = require('sinon');
+const expect = require('chai').expect;
+
+describe('sendPaymentRequestToApi', () => {
+    let consoleSpy;
+
+    beforeEach(() => {
+        consoleSpy = sinon.spy(console, 'log');
+    });
+
+    afterEach(() => {
+        consoleSpy.restore();
+    });
+
+    it('should log the correct total and be called once usins 100, 20', () => {
+        sendPaymentRequestToApi(100, 20);
+
+        expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+        expect(consoleSpy.calledOnce).to.be.true
+    });
+
+    it('should log the correct total and be called once usins 10,10', () => {
+        sendPaymentRequestToApi(10, 10);
+
+        expect(consoleSpy.calledWith('The total is: 20')).to.be.true;
+        expect(consoleSpy.calledOnce).to.be.true
+    })
+})
